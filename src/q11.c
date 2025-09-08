ca28cb02248b29a191f083c79360c402deffe2ea
Sujal1323 <Sujal1323.sujalbc49@gmail.com>
2025-09-08 10:06:44 +0545

    assignment complete

--- src/q11.c
+++ src/q11.c
@@ -1,5 +1,20 @@
-/* Write a C program that prints a pattern of asterisks.
-    *****
-     ***
-      *
-*/
+#include <stdio.h>
+int main() {
+    int i, j, space;
+    int rows = 3;
+
+    for (i = rows; i >= 1; i--) {
+        
+        for (space = 1; space <= rows - i; space++) {
+            printf(" ");
+        }
+    
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
--- src/q4.c
+++ src/q4.c
@@ -1,1 +1,11 @@
-// Write a C program that prints all even numbers between 1 and 100 using a for loop.
+#include<stdio.h>
+int main(){
+    int i;
+    for ( i = 2; i <100; i+=2)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+    
+
+}
